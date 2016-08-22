b6c693f17969a46a00a495a42933a79c4eb6ca85
Andrey Smetanin <irqlevel@gmail.com>
2016-08-23 00:51:30 +0300

    error: add tab
    Signed-off-by: Andrey Smetanin <irqlevel@gmail.com>

--- kstorage/error.h
+++ kstorage/error.h
@@ -1,8 +1,8 @@
 #pragma once
 
-#define E_BASE 0xFFFE0000
+#define E_BASE      0xFFFE0000
 
-#define E_OK 0
-#define E_INVAL (E_BASE + 1)
-#define E_NO_MEM (E_BASE + 2)
-#define E_CANCELED (E_BASE + 3)
+#define E_OK        0
+#define E_INVAL     (E_BASE + 1)
+#define E_NO_MEM    (E_BASE + 2)
+#define E_CANCELED  (E_BASE + 3)
